832ffab7ca060de5db604fba404bd40cb1a1f9ce
Manish-98 <41546440+Manish-98@users.noreply.github.com>
2026-09-23 01:53:15 +0530

    Fix About renderer to use shared HTML escaping

--- games/about.js
+++ games/about.js
@@ -1,3 +1,5 @@
+import { escapeHtml } from '../dom.js';
+
 const ABOUT_SECTIONS = [
   ['objective', 'Objective'],
   ['concept', 'Concept'],
@@ -7,15 +9,6 @@ const ABOUT_SECTIONS = [
   ['controls', 'Controls'],
   ['scoring', 'Scoring']
 ];
-
-function escapeHtml(value) {
-  return String(value)
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#039;');
-}
 
 export function renderAbout(about) {
   if (!about) return '';
