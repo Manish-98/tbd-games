2599634b309ee26edd6b2689914334bd808bf731
Manish-98 <41546440+Manish-98@users.noreply.github.com>
2026-09-23 01:42:27 +0530

    feat: add shared game about renderer

---
+++ games/about.js
@@ -0,0 +1,37 @@
+const ABOUT_SECTIONS = [
+  ['objective', 'Objective'],
+  ['concept', 'Concept'],
+  ['howToPlay', 'How to play'],
+  ['rules', 'Rules'],
+  ['components', 'Components'],
+  ['controls', 'Controls'],
+  ['scoring', 'Scoring']
+];
+
+function escapeHtml(value) {
+  return String(value)
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#039;');
+}
+
+export function renderAbout(about) {
+  if (!about) return '';
+  return `<section class="game-about" aria-labelledby="game-about-heading">
+    <div class="about-heading">
+      <p class="section-label">About this game</p>
+      <h3 id="game-about-heading">Learn the rules.<br />Then play with intent.</h3>
+    </div>
+    <div class="about-content">
+      ${ABOUT_SECTIONS.map(([key, title]) => {
+        const value = about[key];
+        const content = Array.isArray(value)
+          ? `<ul>${value.map((item) => `<li>${escapeHtml(item)}</li>`).join('')}</ul>`
+          : `<p>${escapeHtml(value)}</p>`;
+        return `<article class="about-section"><h4>${title}</h4>${content}</article>`;
+      }).join('')}
+    </div>
+  </section>`;
+}
